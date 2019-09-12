e93040a2415eeb9692bf23b2ddf5c1637f36edce
RichStone <richard.had@hotmail.de>
2019-09-12 19:01:35 +0200

    merge

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,13 +4,13 @@ import "firebaseui/dist/firebaseui.css";
 import 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appID: "app-id",
+  apiKey: "AIzaSyDyFhUD9f3XQqdUEbIevt7hsZc7LcB7hYs",
+  authDomain: "trackomad.firebaseapp.com",
+  databaseURL: "https://trackomad.firebaseio.com",
+  projectId: "trackomad",
+  storageBucket: "trackomad.appspot.com",
+  messagingSenderId: "223356418198",
+  appId: "1:223356418198:web:0bd46a93b4baee385deab7"
 };
 firebase.initializeApp(firebaseConfig)
 
